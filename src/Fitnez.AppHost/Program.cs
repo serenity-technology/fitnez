6c6f988b0ea5c4e81f1f8ed3da14b0b20de17a80
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-12-16 11:29:44 +0200

    Add more projects

--- src/Fitnez.AppHost/Program.cs
+++ src/Fitnez.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Fitnez_Web>("fitnez-web");
 
+builder.AddProject<Projects.Fitnez_App>("fitnez-app");
+
 builder.Build().Run();
