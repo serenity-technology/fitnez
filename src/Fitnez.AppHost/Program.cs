134e6154f7ca7e80d7635f9435180fe34a90e1cc
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-12-20 07:28:57 +0200

    Add identity project

--- src/Fitnez.AppHost/Program.cs
+++ src/Fitnez.AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.Fitnez_Web>("web");
 
 builder.AddProject<Projects.Fitnez_App>("app");
 
+builder.AddProject<Projects.Fitnez_Identity>("identity");
+
 builder.Build().Run();
---
+++ src/Fitnez.Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fitnez.Identity.Pages.Account;
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
